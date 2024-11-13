502bd5a877520785e64d0b800c4a418d1c0c8a66
guk001 <guk001@ucsd.edu>
2024-11-12 19:44:38 -0800

    add sharehi_logo in public folder and homepage

--- my-app/src/pages/Home.js
+++ my-app/src/pages/Home.js
@@ -7,8 +7,13 @@ const Home = () => {
       <>
       <LandingHeader />
         <main className="flex justify-center items-center w-full h-full min-h-screen text-center">
-        <div className="container h-full">
-        <h2 className="text-3xl text-customGreen font-semibold mb-10 ml-12">Welcome to SHARE HI!</h2>
+        <div className="container h-full flex flex-col items-center">
+        <img 
+        src="/sharehi_logo.jpeg"
+        alt="share_hi"
+        className="w-40 h-40 mb-6 object-contain">
+        </img>
+        <h2 className="text-3xl text-customGreen font-semibold mb-10">Welcome to SHARE HI!</h2>
         </div>
         </main>
       <LandingFooter />
