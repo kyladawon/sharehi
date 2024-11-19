540662f9ce2c864038977cd1a414439e102abc81
guk001 <guk001@ucsd.edu>
2024-11-18 20:29:22 -0800

    fix error with .env

--- my-app/src/firebase.js
+++ my-app/src/firebase.js
@@ -1,5 +1,4 @@
 // src/firebase.js
-import dotenv from 'dotenv';
 // import admin from 'firebase-admin';
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
@@ -8,12 +7,12 @@ import { addDoc, collection } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 // admin.initializeApp({
