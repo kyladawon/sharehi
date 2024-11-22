57701cd70ea48da3f688d2807b04b9b66c8d61fb
kyladawon <parkdawon5@gmail.com>
2024-11-22 12:50:39 -0800

    added env

--- my-app/src/firebase.js
+++ my-app/src/firebase.js
@@ -2,7 +2,6 @@
 
 // Import the Firebase functions
 import { initializeApp } from "firebase/app";
-//import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
@@ -18,7 +17,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-//const analytics = getAnalytics(app);
 
 // Firebase services
 const auth = getAuth(app);
